8b7fca8b1840a890a8d2d7fa7b903c21481cb713
yuya <kinjo.bobchin@gmail.com>
2024-05-24 12:44:35 +0900

    chore: Update Jest configuration and VSCode settings

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  coverageProvider: 'v8',
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  transformIgnorePatterns: ['/node_modules/'],
-};
